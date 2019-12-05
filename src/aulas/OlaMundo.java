e2348ede4bbc312a5452aca59f4cce08870c91fa
Moises <moisesmatsinhe>
2019-12-05 16:41:57 +0200

    Outra maneira de imprimir dados
    Usei o JOptionPane para imprimir ola mundo

--- src/aulas/OlaMundo.java
+++ src/aulas/OlaMundo.java
@@ -1,6 +1,8 @@
 
 package aulas;
 
+import javax.swing.JOptionPane;
+
 
 public class OlaMundo {
 
@@ -8,6 +10,7 @@ public class OlaMundo {
     public static void main(String[] args) {
       
         System.out.println("Ola Mundo!!");
+        JOptionPane.showMessageDialog(null, "Ola Mundo!!");
     }
     
 }
