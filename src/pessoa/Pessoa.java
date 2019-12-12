3b4dbca959ff6fd1099ce9f8cf5484985086b2f5
Moises <moisesmatsinhe>
2019-12-12 14:04:05 +0200

    Criarcao da Classe Pessoa e seus Abributos

---
+++ src/pessoa/Pessoa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pessoa;
+
+/**
+ * Faça um programa para representar a árvore genealógica de uma família. Para
+tal, crie uma classe Pessoa que permita indicar, além de nome e idade, o pai e a
+mãe. Tenha em mente que pai e mãe também são do tipo Pessoa.
+ * 
+ */
+public class Pessoa {
+    private String nome;
+    private String idade;
+
+}
